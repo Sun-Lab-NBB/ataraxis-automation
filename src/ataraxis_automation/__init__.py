5c5ed6b2763b7b544e5b0b70e19c1dc6b53be1c8
Inkaros <ikondratv@gmail.com>
2024-07-21 18:09:54 -0400

    Reworked main library __init__() to include author and helper links

--- src/ataraxis_automation/__init__.py
+++ src/ataraxis_automation/__init__.py
@@ -1,10 +1,7 @@
 """This library exposes functions used by tox tasks to automate project development and provides a cli-interface for
 calling these functions.
 
-The library is not designed to be used directly. Instead, it is meant to be used with 'tox' and other helper packages
-like 'mypy', 'ruff', 'stubgen'. The functions exposed through this library augment the functionality of these
-helpers by either setting-up, tearing-down or otherwise supporting their runtime.
-
-All functions in this library assume the project they work with is formatted according to Sun Lab standards. Therefore,
-significant refactoring may be required to use this library for projects that do not follow those standards.
+See https://github.com/Sun-Lab-NBB/ataraxis-automation for more details.
+API documentation: https://ataraxis-automation-api-docs.netlify.app/
+Author: Ivan Kondratyev (Inkaros)
 """
